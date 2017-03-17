8d85ab4c37318c139fe76335ee57cd5a25957abc
Sam Schwartz <schwartz.sam370@gmail.com>
2017-03-17 16:18:54 -0600

    Qualification matches for Utah regional outputting to console works

--- Scouting/src/frc/team1339/master/Main.java
+++ Scouting/src/frc/team1339/master/Main.java
@@ -8,10 +8,20 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
+import frc.team1339.quals.QualsMatch;
+
 public class Main {
 
-	static ArrayList arr = new ArrayList<String>();
-	static ArrayList quals = new ArrayList<QualsMatch>();
+	static ArrayList<Object> teamNames = new ArrayList<Object>();
+	static ArrayList<Object> redOne = new ArrayList<Object>();
+	static ArrayList<Object> redTwo = new ArrayList<Object>();
+	static ArrayList<Object> redThree = new ArrayList<Object>();
+	static ArrayList<Object> blueOne = new ArrayList<Object>();
+	static ArrayList<Object> blueTwo = new ArrayList<Object>();
+	static ArrayList<Object> blueThree = new ArrayList<Object>();
+	static ArrayList<Object> redScore = new ArrayList<Object>();
+	static ArrayList<Object> blueScore = new ArrayList<Object>();
+	static ArrayList<QualsMatch> quals = new ArrayList<QualsMatch>();
 	static Elements temp;
 	static Document doc;
 	
@@ -23,16 +33,38 @@ public class Main {
 		catch(IOException e){
 			e.printStackTrace();
 		}
-		getTeamOne();
-		for(int i = 0; i < arr.size(); i++){
-			System.out.println(arr.get(i));
-			//System.out.println("Red Quals " + (i+1) + " " + arr.get(i));
+		generateQual();
+		for(int i = 0; i < 160; i++){
+			if(i % 2 == 0){
+				System.out.println("Quals Match " + ((i/2) + 1));
+			}
+			System.out.println(quals.get(i).toString());
+			if(i % 2 == 1){
+				System.out.println();
+			}
 		}
 	}
+	
+	public static void generateQual(){
+		getTeamNames();
+		getTeamOne();
+		getTeamTwo();
+		getTeamThree();
+		getTeamFour();
+		getTeamFive();
+		getTeamSix();
+		getRedScore();
+		getBlueScore();
+		for(int i = 0; i < 80; i++){
+			quals.add(new QualsMatch(true, redOne.get(i), redTwo.get(i), redThree.get(i), redScore.get(i)));
+			quals.add(new QualsMatch(false, blueOne.get(i), blueTwo.get(i), blueThree.get(i), blueScore.get(i)));
+		}
+	}
+	
 	public static void getTeamNames(){
 		temp = doc.select("div.team-name");
 		for(Element teamList:temp){
-			arr.add(teamList.getElementsByTag("a").first().text());
+			teamNames.add(teamList.getElementsByTag("a").first().text());
 		}
 	}
 	
@@ -45,7 +77,7 @@ public class Main {
 			if(i % 3 == 0) bool = !bool;
 			if(bool) {
 				if(j % 3 == 0){ //==1 for second, ==2 for third
-					arr.add(teamList.getElementsByTag("a").first().text());
+					redOne.add(teamList.getElementsByTag("a").first().text());
 				}
 			}
 			j++;
@@ -61,13 +93,14 @@ public class Main {
 			if(i % 3 == 0) bool = !bool;
 			if(bool) {
 				if(j % 3 == 1){
-					arr.add(teamList.getElementsByTag("a").first().text());
+					redTwo.add(teamList.getElementsByTag("a").first().text());
 				}
 			}
 			j++;
 			i++;
 		}
 	}
+	
 	public static void getTeamThree(){
 		temp = doc.select("td.red");
 		int i = 0;
@@ -77,7 +110,7 @@ public class Main {
 			if(i % 3 == 0) bool = !bool;
 			if(bool) {
 				if(j % 3 == 2){
-					arr.add(teamList.getElementsByTag("a").first().text());
+					redThree.add(teamList.getElementsByTag("a").first().text());
 				}
 			}
 			j++;
@@ -85,28 +118,75 @@ public class Main {
 		}
 	}
 	
+	public static void getTeamFour(){
+		temp = doc.select("td.blue");
+		int i = 0;
+		int j = 0;
+		boolean bool = true;
+		for(Element teamList:temp){
+			if(i % 3 == 0) bool = !bool;
+			if(bool) {
+				if(j % 3 == 0){ //==1 for second, ==2 for third
+					blueOne.add(teamList.getElementsByTag("a").first().text());
+				}
+			}
+			j++;
+			i++;
+		}
+	}
+	
+	public static void getTeamFive(){
+		temp = doc.select("td.blue");
+		int i = 0;
+		int j = 0;
+		boolean bool = true;
+		for(Element teamList:temp){
+			if(i % 3 == 0) bool = !bool;
+			if(bool) {
+				if(j % 3 == 1){ //==1 for second, ==2 for third
+					blueTwo.add(teamList.getElementsByTag("a").first().text());
+				}
+			}
+			j++;
+			i++;
+		}
+	}
+	
+	public static void getTeamSix(){
+		temp = doc.select("td.blue");
+		int i = 0;
+		int j = 0;
+		boolean bool = true;
+		for(Element teamList:temp){
+			if(i % 3 == 0) bool = !bool;
+			if(bool) {
+				if(j % 3 == 2){ //==1 for second, ==2 for third
+					blueThree.add(teamList.getElementsByTag("a").first().text());
+				}
+			}
+			j++;
+			i++;
+		}
+	}
 	public static void getRedScore(){
 		temp = doc.select("td.redScore");
 		int i = 0;
 		for(Element teamList:temp){
 			if(i % 2 == 1){
-				arr.add(teamList.getElementsByTag("span").first().text());
+				redScore.add(teamList.getElementsByTag("span").first().text());
 			}
 			i++;
 		}
 	}
 	
-	public static class QualsMatch{
-		boolean red;
-		int team1, team2, team3;
-		int score;
-		
-		public QualsMatch(boolean red, int team1, int team2, int team3, int score){
-			this.red = red;
-			this.team1 = team1;
-			this.team2 = team2;
-			this.team3 = team3;
-			this.score = score;
+	public static void getBlueScore(){
+		temp = doc.select("td.blueScore");
+		int i = 0;
+		for(Element teamList:temp){
+			if(i % 2 == 1){
+				blueScore.add(teamList.getElementsByTag("span").first().text());
+			}
+			i++;
 		}
 	}
 }
